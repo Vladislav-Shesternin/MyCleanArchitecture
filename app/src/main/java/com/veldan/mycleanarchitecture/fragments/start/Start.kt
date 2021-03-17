59c08ccfca54c004db88ad1b8aa9b65868cbc661
Vlad Shesternin <valdon1202@gmail.com>
2021-03-17 13:28:44 +0200

    Set up the start configuration of the project.

---
+++ app/src/main/java/com/veldan/mycleanarchitecture/fragments/start/Start.kt
@@ -0,0 +1,28 @@
+package com.veldan.mycleanarchitecture.fragments.start
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.veldan.mycleanarchitecture.databinding.FragmentStartBinding
+
+class Start : Fragment() {
+
+    private lateinit var binding: FragmentStartBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        initBinding()
+        return binding.root
+    }
+
+    // {init}: Binding
+    private fun initBinding() {
+        binding = FragmentStartBinding.inflate(layoutInflater)
+    }
+
+}
